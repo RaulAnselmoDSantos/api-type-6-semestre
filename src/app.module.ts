7c07701c36bbafd0d1ce7f704666d8db948c4075
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-11-18 00:56:07 -0300

    feat: nest created new CRUDS with 6 comands

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module'; // Importar o PrismaModule
-import { UsuarioService } from './user/usuario.service';
-import { UsuarioController } from './user/usuario.controller';
 import { AppControllerAPI } from './app.controller';
+
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+
 
 @Module({
   imports: [PrismaModule], // Registrar o PrismaModule
-  controllers: [AppController,UsuarioController, AppControllerAPI],
-  providers: [AppService, UsuarioService],
+  controllers: [AppController,UserController, AppControllerAPI],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
