0dbec9368edb26fdc4e711c0e98f56fc6974ff41
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2025-08-03 21:12:23 -0300

    refactor: remove ESLint and Prettier configuration files; update database URL in schema

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,23 +4,23 @@ import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('login')
-    async login(@Body() loginDto: LoginDto) {
-        console.log('Recebido no Controller:', loginDto);
-        console.log('Email recebido:', loginDto.email_usuario);
-        console.log('Senha recebida:', loginDto.senha_usuario);
-        if (!loginDto.senha_usuario || !loginDto.email_usuario) {
-            throw new UnauthorizedException('Email e senha são obrigatórios.');
-        }
-        const user = await this.authService.validateUser(
-            loginDto.email_usuario,
-            loginDto.senha_usuario,
-        );
-        if (!user) {
-            throw new UnauthorizedException('Credenciais inválidas');
-        }
-        return this.authService.login(loginDto);
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    console.log('Recebido no Controller:', loginDto);
+    console.log('Email recebido:', loginDto.email_usuario);
+    console.log('Senha recebida:', loginDto.senha_usuario);
+    if (!loginDto.senha_usuario || !loginDto.email_usuario) {
+      throw new UnauthorizedException('Email e senha são obrigatórios.');
     }
+    const user = await this.authService.validateUser(
+      loginDto.email_usuario,
+      loginDto.senha_usuario,
+    );
+    if (!user) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+    return this.authService.login(loginDto);
+  }
 }
